eba198431ba95882a09c2d8174fd35cc99b92c36
Zitr0nenmelisse <31442629+Zitr0nenmelisse@users.noreply.github.com>
2020-05-06 18:15:50 +0200

    Update he_accel.cpp

--- he_accel.cpp
+++ he_accel.cpp
@@ -1,10 +1,10 @@
 #include "he_accel.h"
 
 
-void tLweFFTAddMulRTo(hls::stream<cmpxDataIn> &A, hls::stream<cmpxDataIn> &B,  hls::stream<cmpxDataOut> &out) {
+void tLweFFTAddMulRTo(hls::stream<custom_axi_type> &A, hls::stream<custom_axi_type> &B,  hls::stream<custom_axi_type> &out) {
 
-	cmpxDataOut Aa, Bb;
-	cmpxDataOut valOut;
+	custom_axi_type Aa, Bb;
+	custom_axi_type valOut;
 
 	cmpxDataOut C[1024];
 	for (int i = 0; i < 1024; i++)
@@ -15,51 +15,62 @@ void tLweFFTAddMulRTo(hls::stream<cmpxDataIn> &A, hls::stream<cmpxDataIn> &B,  h
 		for (int i = 0; i < 1024; ++i) {
 			A >> Aa;
 			B >> Bb;
-			C[i] += Aa * Bb;
+			C[i] += Aa.data * Bb.data;
 		}
 	}
 
 	// stream back the results
 	for (int j=0; j<FFT_LENGTH; j++){
-		out.write(C[j]);
+		valOut.data = C[j];
+		valOut.last = (j == 1023)? 1 : 0;
+		out.write(valOut);
 	}
 }
 
 void dummy_proc_fe(
     bool direction,
     config_t* config,
-	hls::stream<cmpxDataIn> &in,
+	hls::stream<custom_axi_type> &in,
     cmpxDataIn out[FFT_LENGTH])
 {
     int i;
+    custom_axi_type valIn;
     config->setDir(direction);
     config->setSch(0x2AB);
-    for (i=0; i< FFT_LENGTH; i++)
-    	in.read(out[i]);
+    for (i=0; i< FFT_LENGTH; i++) {
+    	in.read(valIn);
+    	out[i] = valIn.data;
+    }
 }
 
 void dummy_proc_fe1(
     bool direction,
     config_t* config,
-	hls::stream<cmpxDataIn> &in,
+	hls::stream<custom_axi_type> &in,
     cmpxDataIn out[FFT_LENGTH])
 {
     int i;
+    custom_axi_type valIn;
     config->setDir(direction);
     config->setSch(0x2AB);
-    for (i=0; i< FFT_LENGTH; i++)
-    	in.read(out[i]);
+    for (i=0; i< FFT_LENGTH; i++) {
+    	in.read(valIn);
+    	out[i] = valIn.data;
+    }
 }
-
 void dummy_proc_be(
     status_t* status_in,
     bool* ovflo,
     cmpxDataOut in[FFT_LENGTH],
-	hls::stream<cmpxDataOut> &out)
+	hls::stream<custom_axi_type> &out)
 {
     int i;
-    for (i=0; i< FFT_LENGTH; i++)
-        out.write(in[i]);
+    custom_axi_type valOut;
+    for (i=0; i< FFT_LENGTH; i++) {
+    	valOut.data = in[i];
+    	valOut.last = (i == 1023)? 1 : 0;
+    	out.write(valOut);
+    }
     *ovflo = status_in->getOvflo() & 0x1;
 }
 
@@ -67,15 +78,18 @@ void dummy_proc_be1(
     status_t* status_in,
     bool* ovflo,
     cmpxDataOut in[FFT_LENGTH],
-	hls::stream<cmpxDataOut> &out)
+	hls::stream<custom_axi_type> &out)
 {
     int i;
-    for (i=0; i< FFT_LENGTH; i++)
-        out.write(in[i]);
+    custom_axi_type valOut;
+    for (i=0; i< FFT_LENGTH; i++) {
+    	valOut.data = in[i];
+    	valOut.last = (i == 1023)? 1 : 0;
+    	out.write(valOut);
+    }
     *ovflo = status_in->getOvflo() & 0x1;
 }
-
-void fft_top(hls::stream<cmpxDataIn> &in, hls::stream<cmpxDataOut> &out, bool direction, bool* ovflo) {
+void fft_top(hls::stream<custom_axi_type> &in, hls::stream<custom_axi_type> &out, bool direction, bool* ovflo) {
 
 	cmpxDataIn xn[FFT_LENGTH];
 	cmpxDataOut xk[FFT_LENGTH];
@@ -99,7 +113,7 @@ void fft_top(hls::stream<cmpxDataIn> &in, hls::stream<cmpxDataOut> &out, bool di
 
 }
 
-void ifft_top(hls::stream<cmpxDataIn> &in, hls::stream<cmpxDataOut> &out, bool direction, bool* ovflo) {
+void ifft_top(hls::stream<custom_axi_type> &in, hls::stream<custom_axi_type> &out, bool direction, bool* ovflo) {
 
 	cmpxDataIn xn1[FFT_LENGTH];
 	cmpxDataOut xk1[FFT_LENGTH];
@@ -121,15 +135,17 @@ void ifft_top(hls::stream<cmpxDataIn> &in, hls::stream<cmpxDataOut> &out, bool d
 
 }
 
-void top_function(hls::stream<cmpxDataIn> &deca, hls::stream<cmpxDataIn> &gsw, hls::stream<cmpxDataOut> &outStream) {
+
+void top_function(hls::stream<custom_axi_type> &deca, hls::stream<custom_axi_type> &gsw, hls::stream<custom_axi_type> &outStream) {
 
 #pragma HLS INTERFACE axis port=outStream
 #pragma HLS INTERFACE axis port=deca
 #pragma HLS INTERFACE axis port=gsw
 #pragma HLS INTERFACE s_axilite port=return bundle=ctrl
 
-	hls::stream<cmpxDataOut> decaFFT;
-	hls::stream<cmpxDataOut> tmpa;
+	hls::stream<custom_axi_type> decaFFT;
+	hls::stream<custom_axi_type> tmpa;
+	//hls::stream<custom_axi_type> outSt;
 	bool ovflo;
 
 	// fft deca
@@ -138,6 +154,7 @@ void top_function(hls::stream<cmpxDataIn> &deca, hls::stream<cmpxDataIn> &gsw, h
 	tLweFFTAddMulRTo(decaFFT, gsw, tmpa);
 
 	// inverse FFT on tmpa
+	//void ifft_top(hls::stream<custom_axi_type>, hls::stream<cmpxDataOut>, bool , bool*); <- earlier version
 	ifft_top(tmpa, outStream, true, &ovflo);
 
 }
