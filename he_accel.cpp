bcfc0adfbf34f418ddb9030d0043a1783d9b1160
coolhill <8256597+coolhill@users.noreply.github.com>
2020-05-07 19:37:53 +0200

    new complex type

--- he_accel.cpp
+++ he_accel.cpp
@@ -1,70 +1,23 @@
 #include "he_accel.h"
 
-
-void tLweFFTAddMulRTo() {
-
-}
-
-void dummy_proc_fe(
-    bool direction,
-    config_t* config,
-	hls::stream<cmpxDataIn> &in,
-    cmpxDataIn out[FFT_LENGTH])
-{
-    int i;
-    config->setDir(direction);
-    config->setSch(0x2AB);
-    for (i=0; i< FFT_LENGTH; i++)
-    	in.read(out[i]);
-}
-
-void dummy_proc_be(
-    status_t* status_in,
-    bool* ovflo,
-    cmpxDataOut in[FFT_LENGTH],
-	hls::stream<cmpxDataOut> &out)
-{
-    int i;
-    for (i=0; i< FFT_LENGTH; i++)
-        out.write(in[i]);
-    *ovflo = status_in->getOvflo() & 0x1;
-}
-
-void fft_top(hls::stream<cmpxDataIn> &in, hls::stream<cmpxDataOut> &out, bool direction, bool* ovflo) {
-
-	cmpxDataIn xn[FFT_LENGTH];
-	cmpxDataOut xk[FFT_LENGTH];
-
-	config_t fft_config;
-	status_t fft_status;
-
-	dummy_proc_fe(direction, &fft_config, in, xn);
-
-	hls::fft<config1>(xn, xk, &fft_status, &fft_config);
-
-	dummy_proc_be(&fft_status, ovflo, xk, out);
-
-}
-
-
-void top_function(hls::stream<int_axis> &A, hls::stream<int_axis> &B, hls::stream<int_axis> &outStream) {
+void mult(hls::stream<complex_axi_type> &decaFFT, hls::stream<complex_axi_type> &gsw, hls::stream<complex_axi_type> &outStream) {
 
 #pragma HLS INTERFACE axis port=outStream
-#pragma HLS INTERFACE axis port=A
-#pragma HLS INTERFACE axis port=B
+#pragma HLS INTERFACE axis port=decaFFT
+#pragma HLS INTERFACE axis port=gsw
 #pragma HLS INTERFACE s_axilite port=return bundle=ctrl
 
-	int_axis valOut, Aa, Bb;
+	complex_axi_type valOut, Aa, Bb;
 
-	int C[1024];
+	cmpxDataIn C[1024];
 	for (int i = 0; i < 1024; i++)
 		C[i] = 0;
 
 	// do the computation
 	for (int j = 0; j < 6; j++) {
 		for (int i = 0; i < 1024; ++i) {
-			A >> Aa;
-			B >> Bb;
+			decaFFT >> Aa;
+			gsw >> Bb;
 			C[i] += Aa.data * Bb.data;
 		}
 	}
